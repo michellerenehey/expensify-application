f2a14992ac626b11254d084710c31ff537a6ebea
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-06-03 17:18:04 -0700

    Import Expensify colors, reset colors and add basic styling

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>
-        Expensify's Next <br />
+      <h1>Expensify's Next</h1>
+      <h2>
         <em>React Full Stack Engineer</em>
-      </h1>
-      <img src={me} className="App-logo" alt="me with dahlia" />
-      <div>
-        <section>First question</section>
-        <section>Second question</section>
-        <section>Third question</section>
+      </h2>
+      <img src={me} className="photo" alt="me with dahlia" />
+      <div className="questions">
+        <section className="question">
+          <h5>
+            What's the URL of your website? If you don't have one, why not?
+          </h5>
+        </section>
+        <section className="question">
+          <h5>
+            What's your coding history? When did you start, and what have you
+            done between then and now?
+          </h5>
+        </section>
+        <section className="question">
+          <h5>
+            What do you want to do with the rest of your life, and how is
+            Expensify a step toward your long-term goals?
+          </h5>
+        </section>
       </div>
       <a
-        className="App-link"
+        className="link"
         href="https://www.michellenygren.dev/"
         target="_blank"
         rel="noopener noreferrer"
