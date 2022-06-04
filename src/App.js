a8d8088aebe16fc23f72a69c7f2667bf7d1a20ef
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-06-03 17:33:52 -0700

    Add expand state for answers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import me from "./assets/me.png";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <div className="App">
       <h1>Expensify's Next</h1>
@@ -10,10 +17,24 @@ function App() {
       </h2>
       <img src={me} className="photo" alt="me with dahlia" />
       <div className="questions">
-        <section className="question">
+        <section className="question" onClick={handleExpand}>
           <h5>
             What's the URL of your website? If you don't have one, why not?
           </h5>
+          <p className="arrow" onClick={handleExpand}>
+            ▼
+          </p>
+          {isExpanded && (
+            <>
+              <a
+                href="https://www.michellenygren.dev/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                www.michellenygren.dev
+              </a>
+            </>
+          )}
         </section>
         <section className="question">
           <h5>
